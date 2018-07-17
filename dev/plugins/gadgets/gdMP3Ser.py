b780ebc64a60a3d595faa28169fc6fd91e867712
JK <jk@nerd2nerd.org>
2018-07-17 23:55:06 +0200

    finished MP3-layer

--- dev/plugins/gadgets/gdMP3Ser.py
+++ dev/plugins/gadgets/gdMP3Ser.py
@@ -45,9 +45,8 @@ class PluginGadget(GS):
             self._ser.init(9600, 8, None, 1) # baud=9600 databits=8 parity=none stopbits=1
 
             self.param['Mode'] = int(self.param['Mode'])
-            #self.send_command(0x09, 2)   # Storage Device = SD
             self.set_volume(int(self.param['Volume']))   # Set volume
-            self.play_index(1)   # Play
+            #self.play_index(1)   # Play
 
 # -----
 
@@ -65,9 +64,9 @@ class PluginGadget(GS):
                 if type(val) is str:
                     if val.find(':') > 0:
                         val = val.split(':', 1)
-                        ndir = int(val[0]) & 0xFF
-                        file = int(val[1]) & 0xFF
-                        self.play_folder(0x0F, ndir, file)   # Play Folder
+                        ndir = int(val[0])
+                        file = int(val[1])
+                        self.play_folder(ndir, file)   # Play Folder
                     else:
                         val = int(val)
                         self.play_index(val)   # Play Index
@@ -75,39 +74,6 @@ class PluginGadget(GS):
                     self.play_index(val)   # Play Index
         except:
             pass
-
-# -----
-
-    def timer(self, prepare:bool):
-        while self.process():
-            pass
-            #print(self.value, self.unit, self.form)
-            #key = self.param['RespVar']
-            #source = self.param['NAME']
-            #Variable.set(key, self.value, source)
-
-# =====
-
-    def is_valid(self):
-        if self.data[0] != 0x7E:   # Starting
-            return False
-        if self.data[1] != 0xFF:   # Version
-            return False
-        if self.data[2] != 0x06:   # Len
-            return False
-        if self.data[9] != 0xEF:   # End
-            return False
-        #sum = 0
-        #for i in range(1, 8):
-        #    sum -= self.data[i]
-        #if self.data[xxx] != (sum & 0xFF):    # Checksum
-        #    return False
-        return True
-
-# -----
-
-    def interpret(self):
-        self._remove_data(self.packet_size)
 
 # =====
 
@@ -157,29 +123,34 @@ class PluginGadget(GS):
 # -----
 
     def play_index(self, val:int):
+        if not 1 <= val <= 2000:
+            return
         if self.param['Mode'] == 1:
-            self.send_command_short()(0x41, val)   # Play
+            self.send_command_short(0x41, val)   # Play
         else:
             self.send_command_long(0x03, val)   # Play
-        pass
 
 # -----
 
     def play_folder(self, ndir:int, file:int):
+        if not 1 <= ndir <= 99:
+            return
+        if not 1 <= file <= 255:
+            return
         val = ndir << 8 | file
         if self.param['Mode'] == 1:
             self.send_command_short(0x42, val)   # Play Folder
         else:
             self.send_command_long(0x0F, val)   # Play Folder
-        pass
 
 # -----
 
     def set_volume(self, val:int):
+        if not 1 <= val <= 30:
+            return
         if self.param['Mode'] == 1:
             self.send_command_short(0x31, val, 5)   # Set volume
         else:
             self.send_command_long(0x06, val)   # Set volume
-        pass
 
 #######
